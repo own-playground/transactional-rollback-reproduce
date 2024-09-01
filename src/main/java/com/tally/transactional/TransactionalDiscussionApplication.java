a0a69814cb957ccd4bc02eaf493b14388c6dbaf5
ahn-sj <jaesa5221@gmail.com>
2024-09-01 20:50:55 +0900

    Init: Project Initial

---
+++ src/main/java/com/tally/transactional/TransactionalDiscussionApplication.java
@@ -0,0 +1,13 @@
+package com.tally.transactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalDiscussionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalDiscussionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tally/transactional/TransactionalDiscussionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tally.transactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalDiscussionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
